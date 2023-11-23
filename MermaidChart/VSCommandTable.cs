5ee7c7f2af43e595e37206ec758c485d0708d203
Daniil Kurchanov <dkurch84@gmail.com>
2023-11-23 13:13:44 +0300

    Update icons

--- MermaidChart/VSCommandTable.cs
+++ MermaidChart/VSCommandTable.cs
@@ -18,14 +18,11 @@ namespace MermaidChart
         public const string DiagramListWindowCmdSetString = "c74abe73-65d7-4e96-b2f4-30c8354348ed";
         public static Guid DiagramListWindowCmdSet = new Guid(DiagramListWindowCmdSetString);
 
-        public const string guidMermaidChartPackageCmdSetString = "512fc355-237a-4937-9805-cbdb25e11682";
-        public static Guid guidMermaidChartPackageCmdSet = new Guid(guidMermaidChartPackageCmdSetString);
+        public const string MermaidChartCommandSetString = "512fc355-237a-4937-9805-cbdb25e11682";
+        public static Guid MermaidChartCommandSet = new Guid(MermaidChartCommandSetString);
 
-        public const string guidImagesString = "0539ae13-0390-4fb5-a266-96ed3eb9cd16";
-        public static Guid guidImages = new Guid(guidImagesString);
-
-        public const string windowImages1String = "eef6e5f7-721b-4283-855a-5dfd714f4e36";
-        public static Guid windowImages1 = new Guid(windowImages1String);
+        public const string DiagramListWindowBitmapString = "eef6e5f7-721b-4283-855a-5dfd714f4e36";
+        public static Guid DiagramListWindowBitmap = new Guid(DiagramListWindowBitmapString);
     }
     /// <summary>
     /// Helper class that encapsulates all CommandIDs uses across VS Package.
@@ -35,19 +32,8 @@ namespace MermaidChart
         public const int DiagramListToolbar = 0x1000;
         public const int DiagramListToolbarGroup = 0x1050;
         public const int MyMenuGroup = 0x1020;
-        public const int cmdidCommand1 = 0x0100;
-        public const int cmdidToolWindow1Command = 0x1021;
-        public const int bmpPic1 = 0x0001;
-        public const int bmpPic2 = 0x0002;
-        public const int bmpPicSearch = 0x0003;
-        public const int bmpPicX = 0x0004;
-        public const int bmpPicArrows = 0x0005;
-        public const int bmpPicStrikethrough = 0x0006;
-        public const int toolWindowPic1 = 0x0001;
-        public const int toolWindowPic2 = 0x0002;
-        public const int toolWindowPicSearch = 0x0003;
-        public const int toolWindowPicX = 0x0004;
-        public const int toolWindowPicArrows = 0x0005;
-        public const int toolWindowPicStrikethrough = 0x0006;
+        public const int RefreshDiagramList = 0x0100;
+        public const int OpenDiagramList = 0x1021;
+        public const int DiagramListWindowIcon = 0x0001;
     }
 }
